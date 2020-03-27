7fa968b92ced507258d1baa19f349a07c8095878
Nicolas Gutierrez <gutigutinico@gmail.com>
2020-03-26 22:35:53 -0500

    Iniciando proyecto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Que mas a todos las personas',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
