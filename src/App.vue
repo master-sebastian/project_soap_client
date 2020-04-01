fa548376b74af59983cbe028a9eb7a7ef6a06457
Nicolas Gutierrez <gutigutinico@gmail.com>
2020-04-01 12:47:32 -0500

    configuracion

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,44 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Bar basico</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarText">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown ml-auto">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Yo
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Cerrar sesion</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <br>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
+
+
+<style>
+  body {
+    background-color: #556;
+    background-size: 100px 140px;
+    background-position: 0 0, 0 0, 40px 70px, 40px 70px, 0 0, 40px 70px;
+    background-image: -webkit-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -webkit-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -webkit-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -webkit-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -webkit-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a), -webkit-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a);
+    background-image: -moz-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -moz-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -moz-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -moz-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -moz-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a), -moz-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a);
+    background-image: -ms-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -ms-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -ms-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -ms-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -ms-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a), -ms-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a);
+    background-image: -o-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -o-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -o-linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -o-linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), -o-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a), -o-linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a);
+    background-image: linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(-45deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a), linear-gradient(30deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a);
+  }
+ 
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,38 +1,72 @@
 <template>
     <div>
-        Que mas a todos {{ message }}
+        <div class="card">
+            <div class="card-header bg-dark text-white">
+                Que mas a todos {{ message }}
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import x2js from 'x2js';
-
+    import axios from 'axios';
     export default {
         name: 'login',
         data() {
             return {
                 message: 'Que mas a todos las personas',
             };
+        },
+        methods: {
+            getXML: function(a, b){
+                    return '\
+                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.webserviceX.NET/">\
+                    <soapenv:Header/>\
+                    <soapenv:Body>\
+                    <__call>\
+                        <method_name>suma</method_name>\
+                        <arguments>\
+                            <a xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:int">\
+                                '+a+'\
+                            </a>\
+                            <b xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:int">\
+                                '+b+'\
+                            </b>\
+                        </arguments>\
+                        <authentication>\
+                            <token>\
+                            </token>\
+                        </authentication>\
+                    </__call>\
+                    </soapenv:Body>\
+                </soapenv:Envelope>';
+            },
+            send: function (){
+                let X2JS = new x2js()
+                axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+                axios.post('http://localhost/projects/soap_project/Route/Test/Test.php',
+                    this.getXML(20, 40),{
+                        headers:{
+                            'Content-Type': 'text/xml',
+                            'Accept': 'text/xml',
+                        }
+                    }).then((res)=>{
+                        let jsonObj = X2JS.xml2js(res.data);
+                        window.test=jsonObj
+                    }).catch((err)=>{
+                        console.log(err)
+                    }
+                );
+            }
+            
         },created(){
-            const xml = '<?xml version="1.0"?>\
-                <soap:Envelope\
-                xmlns:soap="http://www.w3.org/2003/05/soap-envelope/"\
-                soap:encodingStyle="http://www.w3.org/2003/05/soap-encoding">\
-                <soap:Header>\
-                ...\
-                </soap:Header>\
-                <soap:Body>\
-                    1000\
-                <soap:Fault>\
-                ...\
-                </soap:Fault>\
-                </soap:Body>\
-                </soap:Envelope>'
-            console.log(x2js)
-            let codi = new x2js();
-            let document = codi.xml2js(xml);
-            console.log(document.Envelope.Body.toString())
-            //console.log(codi)
+            this.send()
         }
     };
 </script>
