479dbbda391e4f01cf3834e0a1ef1c904abf4dcc
Nicolas Gutierrez <gutigutinico@gmail.com>
2020-04-02 18:13:11 -0500

    Vista

---
+++ src/components/Calc.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-header bg-dark text-white">
+                {{ $data['title'] }}
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="form-group">
+                        <label>Nombre del producto</label>
+                        <input type="text" class="form-control" v-model="articulo">
+                    </div>
+                    <button type="buttom" class="btn btn-primary" @click="agregar()">Agregar</button>
+                </form>
+                <hr>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre del producto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(producto, index) in list" :key="index">
+                            <td>{{ producto }}</td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Calc',
+        data() {
+            return {
+                list: [],
+                articulo: "",
+                title: "Listado de productos a comprar"
+            };
+        },
+        methods: {
+            agregar(){
+                this.list.push(this.articulo)
+            }
+        }
+    }
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import Login from './components/Login.vue'
-
+import Calc from './components/Calc.vue'
 export const routes = [
   {
-    path: '/',
+    path: '/inicio',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/upc-calc',
+    name: 'Calc',
+    component: Calc
   }
 ]
