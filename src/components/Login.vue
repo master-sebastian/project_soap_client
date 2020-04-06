e7f51d429e1fb2bbcfac46ad7ade459d3c51243d
Sebastian G <orjuela.desarrollador@gmail.com>
2020-04-05 21:04:24 -0500

    Calculadora

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,12 +2,19 @@
     <div>
         <div class="card">
             <div class="card-header bg-dark text-white">
-                Que mas a todos {{ message }}
+                Prueba
             </div>
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <form>
+                    <div class="form-group">
+                        <label>Operar</label>
+                        <input type="text" class="form-control" v-model="a">
+                        <input type="text" class="form-control" v-model="b">
+                        <input type="text" class="form-control" v-model="operacion">
+                    </div>
+                    <button type="button" class="btn btn-primary" @click="send()">Agregar</button>
+                    <input type="text" class="form-control" v-model="resultado">
+                </form>
             </div>
         </div>
     </div>
@@ -20,18 +27,22 @@
         name: 'login',
         data() {
             return {
-                message: 'Que mas a todos las personas',
+                a: 0,
+                b: 0,
+                operacion:'',
+                resultado:""
             };
         },
         methods: {
-            getXML: function(a, b){
+            getXML: function(a, b, operacion){
                     return '\
                 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.webserviceX.NET/">\
                     <soapenv:Header/>\
                     <soapenv:Body>\
                     <__call>\
-                        <method_name>suma</method_name>\
+                        <method_name>'+operacion+'</method_name>\
                         <arguments>\
+                            <authentication xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string">15hyhy</authentication>\
                             <a xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:int">\
                                 '+a+'\
                             </a>\
@@ -39,19 +50,20 @@
                                 '+b+'\
                             </b>\
                         </arguments>\
-                        <authentication>\
-                            <token>\
-                            </token>\
-                        </authentication>\
                     </__call>\
                     </soapenv:Body>\
                 </soapenv:Envelope>';
             },
             send: function (){
                 let X2JS = new x2js()
+                let a = this.a
+                let b = this.b
+                let operacion = this.operacion
+                let contenido = this
+                contenido.resultado = ""
                 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
                 axios.post('http://localhost/projects/soap_project/Route/Test/Test.php',
-                    this.getXML(20, 40),{
+                    this.getXML(a, b, operacion),{
                         headers:{
                             'Content-Type': 'text/xml',
                             'Accept': 'text/xml',
@@ -59,6 +71,14 @@
                     }).then((res)=>{
                         let jsonObj = X2JS.xml2js(res.data);
                         window.test=jsonObj
+                        if(jsonObj.Envelope.Body.__callResponse.return['_xsi:type'] == "xsd:int"){
+                            contenido.resultado = parseInt(jsonObj.Envelope.Body.__callResponse.return.__text)
+                        }else if(jsonObj.Envelope.Body.__callResponse.return['_xsi:type'] == "xsd:float"){
+                            contenido.resultado = parseFloat(jsonObj.Envelope.Body.__callResponse.return.__text)
+                        }else{
+                            contenido.resultado = ""
+                        }
+                        
                     }).catch((err)=>{
                         console.log(err)
                     }
